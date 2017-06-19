a24c5d6ac8b6d2c30c748377faba1c37bd91e9cd
budhip <ryo.bunraku@yahoo.com>
2017-06-19 17:21:18 +0700

    commit tugas roman_numeral_recursive.js

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+  if  (input === 0) {
+    return '';
+  }
+
+  let patternRomawi = [
+    [1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],
+    [100, 'C'],[90, 'XC'],[50, 'L'],[40, 'XL'],
+    [10, 'X'],[9, 'IX'],[5, 'V'],[4, 'IV'],[1, 'I']
+  ]
+
+  for (let i = 0; i < patternRomawi.length; i++) {
+    if (input >= patternRomawi[i][0]) {
+      return patternRomawi[i][1] + to_roman(input - patternRomawi[i][0]);
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
