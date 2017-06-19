11bbd12ac1b3d5b961fbc9ca1207e08222062d31
biliman <fickry.bil.iman@gmail.com>
2017-06-19 11:40:29 +0700

    roman numerals recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,11 +1,24 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  let latin = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  
+  if (input < 1) {
+    return '';
+  } else {
+    for (let i = 0; i < latin.length; i++) {
+      if (input >= latin[i]) {
+        return roman[i] + to_roman(input - latin[i])
+      }
+    }
+  }
+  
 }
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
+console.log('3     | III      | ', to_roman(3))
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
