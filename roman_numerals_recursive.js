aa67d6087adf6c9558fa883d00eb4d74e4c281c4
anggraito laras <anggraita.laras@gmail.com>
2017-06-19 21:33:23 +0700

    added task 2

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,13 +1,27 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+
+
+function to_roman(input){
+  var number = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+
+  if(input < 1){
+    return "";
+  }
+  else{
+    for(let i=0; i < number.length ; i++){
+      if(input >= number[i]){
+        return roman[i] + to_roman(input - number[i]);
+      }
+    }
+  }
 }
 
+
 console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('input | expeted| actual')
+console.log('----- | --------| --------')
+console.log('4     | IV      |', to_roman(4))
+console.log('9     | IX      |', to_roman(9))
+console.log('13    | XIII    |', to_roman(13))
+console.log('1453  | MCDLIII |', to_roman(1453))
+console.log('1646  | MDCXLVI |', to_roman(1646))
