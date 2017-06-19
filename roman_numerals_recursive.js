74294ce7239aba957084706e9b3550ae2f38066c
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-06-19 14:48:13 +0700

    roman numeral recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,36 @@
+var romanMatrix = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [3, 'III'],
+  [1, 'I']
+];
+
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  if(input === 0)
+  {
+    return '';
+  }
+  else
+  {
+    for(var i = 0; i < romanMatrix.length; i++)
+    {
+      if(input >= romanMatrix[i][0])
+      {
+        return romanMatrix[i][1] + to_roman(input - romanMatrix[i][0]);
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
