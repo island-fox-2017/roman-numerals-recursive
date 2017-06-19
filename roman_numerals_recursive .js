7bdb1940ecd9546e52c9eb366850505539c3484a
adnin31 <adnin.ris31@gmail.com>
2017-06-19 21:45:35 +0700

    telah selesai mengerjakan tugas roman recursive

--- roman_numerals_recursive .js
+++ roman_numerals_recursive .js
@@ -3,7 +3,6 @@ function to_roman(input) {
   let romanize =''
   let listRoman = ['M','DM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
   let number = [1000 ,900,500,400,100,90,50,40,10,9,5,4,1];
-
       if (input == 0){
         return ''
       } else {
@@ -12,9 +11,7 @@ function to_roman(input) {
             return listRoman[i] + to_roman(input - number[i])
           }
         }
-
       }
-
 }
 
 console.log('My totally sweet testing script for new roman\n')
