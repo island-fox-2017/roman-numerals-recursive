d73fb66b4b2ce706520e8ed1b6d7342958a54491
Tamastro <rpratama45@gmail.com>
2017-06-19 16:49:43 +0700

    TUGAS KEDUA SENIN

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,28 @@
-function to_roman(input) {
+function to_roman(input, isi ='') {
   // start your code here
-  return to_roman(input); 
+  var roman = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ];
+  for (var i = 0; i < roman.length; i++) {
+    if (input >= roman[i][0]){
+      isi += roman[i][1];
+      input = input - roman[i][0];
+      return to_roman(input, isi)
+    }
+  }
+  return isi;
 }
 
 console.log('My totally sweet testing script for new roman\n')
