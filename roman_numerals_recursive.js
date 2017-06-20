aad07596d1cc47bbe5df864bc27d06f18a19569b
aridwia <aridwiananto@gmail.com>
2017-06-20 16:53:14 +0700

    numerals_recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,7 +1,27 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+
+
+function to_roman (num) {
+// your implementation code here
+  word = '';
+
+  var romawi = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+  var desimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
+  for(i=0;i<romawi.length;i++){
+    if(num >= desimal[i]){
+      word += romawi[i];
+      num = num - desimal[i];
+    }
+  }
+
+  if(num === 0){
+    return word
+  }else{
+    return word + to_roman(num)
+  }
+
 }
+
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
