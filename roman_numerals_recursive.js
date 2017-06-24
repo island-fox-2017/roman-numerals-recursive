22bdabf944cc31715b0e3982f47b15ada0658c83
rahid <hidayat.85@gmail.com>
2017-06-24 10:42:07 +0700

    revisi posisi return to_roman(input, result)

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -4,21 +4,22 @@ function to_roman(input, result='') {
   let roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
 
   for (i=arabic.length-1; i>=0; i--) {
+    debugger
     if (input >= arabic[i]) {
       input -= arabic[i];
       result += roman[i];
+      return to_roman(input, result);
     }
     else if (input == 0) {
       return result;
     }
   }
-  return to_roman(input, result);
 }
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('4     | IV       | ', to_roman(4))
+console.log('4     | IV       | ', to_roman(1200))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
